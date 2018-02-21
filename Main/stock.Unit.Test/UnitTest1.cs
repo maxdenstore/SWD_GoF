5c6034f4fc0827c9f332fbf642ce3dd2d4a3392e
Katborg <s.katborg@gmail.com>
2018-02-21 12:33:03 +0100

    Stock unit test

--- Main/stock.Unit.Test/UnitTest1.cs
+++ Main/stock.Unit.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace ObsLib
 {
@@ -16,6 +17,27 @@ namespace ObsLib
         }
 
 
+        [Test]
+        public void StockNameCtorTest()
+        {
+            Assert.That(_uut._name, Is.EqualTo("test"));
+        }
+        [Test]
+        public void StockIdCtorTest()
+        {
+            Assert.That(_uut._id, Is.EqualTo(0));
+        }
 
+        [Test]
+        public void StocAvailibleAmountCtorTest()
+        {
+            Assert.That(_uut._availibleAmount, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void StockNameTest()
+        {
+            Assert.That(_uut._name, Is.EqualTo("test"));
+        }
     }
 }
