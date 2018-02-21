8bce92b14a25a8a7031cda418d7176183d0a3c97
nicho1991 <ladefoged1991@gmail.com>
2018-02-21 11:23:54 +0100

    stock

---
+++ Main/ObsLib/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsLib
+{
+    interface IStock
+    {
+        void attach();
+        void Detach();
+        void notify();
+    }
+    public class Stock
+    {
+    }
+}
