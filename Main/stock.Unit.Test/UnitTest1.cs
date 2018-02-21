6f36317440bc9c6f0139c828253a5935b8fdd6b7
Katborg <s.katborg@gmail.com>
2018-02-21 12:25:00 +0100

    Add test to Stock

---
+++ Main/stock.Unit.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace ObsLib
+{
+    
+    public class StockUnitTest
+    {
+        private Stock _uut;
+
+        [SetUp]
+        public void Init()
+        {
+            _uut = new Stock("test", 8.0, 10);
+        }
+
+
+
+    }
+}
