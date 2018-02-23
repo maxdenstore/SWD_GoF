2fb04d1410bea30e4d62ce76d660f1a600aa301d
Katborg <s.katborg@gmail.com>
2018-02-23 10:25:30 +0100

    portfilie display moved to own file

---
+++ Main/ObsLib/portifolioDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsLib
+{
+
+    public interface IPortifolioDisplay
+    {
+        void print(List<IStock> alistToPrint);
+    }
+
+    public class portifolioDisplay : IPortifolioDisplay
+    {
+        public void print(List<IStock> alistToPrint)
+        {
+            if (alistToPrint.Count == 0)
+            {
+                Console.WriteLine("there is no stocks to print");
+            }
+            foreach (var VARIABLE in alistToPrint)
+            {
+                Console.WriteLine(VARIABLE._name + " user has: " + VARIABLE._availibleAmount + "availible stocks");
+            }
+        }
+    }
+}
