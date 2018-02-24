434afab6cc4ad4015fd726333ebda2c1ae956cd0
nicho1991 <ladefoged1991@gmail.com>
2018-02-24 20:32:33 +0100

    rr

--- Main/ObsLib/PriceChanger.cs
+++ Main/ObsLib/PriceChanger.cs
@@ -6,27 +6,31 @@ namespace ObsLib
     public class PriceChanger
     {
         private static Mutex mut = new Mutex();
-
+        int newPrice;
+        static Random rnd = new Random();
+        private Stock priceUp;
         public void priceChanger(Stock changer)
         {
-            Thread t = new Thread(new ParameterizedThreadStart(TimeThread));
+            priceUp = (Stock)changer;
+            Thread t = new Thread(TimeThread);
             t.Start(changer);
         }
 
         private void TimeThread(object obj)
         {
-            Stock prizeup = (Stock)obj;
-            Random rnd = new Random();
             while (true)
             {
-                int newPrice = (rnd.Next(50, 100));
-                prizeup.Price = newPrice;
-                mut.WaitOne();
-                Console.WriteLine(prizeup.Name + " amount avalible " + prizeup.AvailibleAmount + " new price: " +
-                                  prizeup.Price);
-                mut.ReleaseMutex();
 
                 Thread.Sleep(rnd.Next(1000, 2000));
+                newPrice = rnd.Next(50,100);
+                priceUp.Price = newPrice;
+                
+                mut.WaitOne(100);
+                Console.WriteLine(priceUp.Name + " amount avalible " + priceUp.AvailibleAmount + " new price: " +
+                                  priceUp.Price);
+           
+                mut.ReleaseMutex();
+
 
             }
         }
