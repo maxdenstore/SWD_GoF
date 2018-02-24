0b64697c0561fd8493f3fec67f92dc3abbd9036d
Max <maxbj@live.dk>
2018-02-24 14:00:42 +0100

    deadlocks fixes

--- Main/ObsLib/PriceChanger.cs
+++ Main/ObsLib/PriceChanger.cs
@@ -5,6 +5,8 @@ namespace ObsLib
 {
     public class PriceChanger
     {
+        private static Mutex mut = new Mutex();
+
         public void priceChanger(Stock changer)
         {
             Thread t = new Thread(new ParameterizedThreadStart(TimeThread));
@@ -15,19 +17,25 @@ namespace ObsLib
         {
             while (true)
             {
+                mut.WaitOne();
+
                 Stock prizeup = (Stock) obj;
                 DateTime start = DateTime.Now;
                 Random rnd = new Random();
-                int newPrice = (start.Second)+(rnd.Next(50,100));
+                int newPrice = (start.Second) + (rnd.Next(50, 100));
                 prizeup.Price = newPrice;
 
-                Console.WriteLine(prizeup.Name + " amount avalible " + prizeup.AvailibleAmount + " new price: " + prizeup.Price);
+                Console.WriteLine(prizeup.Name + " amount avalible " + prizeup.AvailibleAmount + " new price: " +
+                                  prizeup.Price);
 
                 prizeup.notify();
 
-                Thread.Sleep(rnd.Next(1000,2000));
-            }
+                Thread.Sleep(rnd.Next(1000, 2000));
 
+                mut.ReleaseMutex();
+
+            }
         }
     }
+
 }
